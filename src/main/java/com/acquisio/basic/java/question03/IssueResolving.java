adef71e582be05da5487b1487e4c9f7f33c392e1
flamelitface <flamelitface@hotmail.com>
2019-06-13 18:34:56 -0400

    Question 3 solution

--- src/main/java/com/acquisio/basic/java/question03/IssueResolving.java
+++ src/main/java/com/acquisio/basic/java/question03/IssueResolving.java
@@ -13,8 +13,19 @@ public class IssueResolving {
         System.out.println(instance.factorial(7));
     }
 
+    /**
+     * This method accepts and returns and int.
+     * Due to overflow issues, this method only operates for values between 0 and 12
+     * @param n must be between 0 and 12
+     * @return n factorial
+     */
     int factorial(int n) {
-        // TODO: Fix code here.
+        if (n < 0 || n > 12) {
+            throw new IllegalArgumentException("This method only accepts positive integers between 1 and 12");
+        }
+        if(n == 0) {
+            return 1;
+        }
         int result = 0;
         result = factorial(n - 1) * n;
         return result;
--- src/test/java/com/acquisio/basic/java/question03/IssueResolvingTest.java
+++ src/test/java/com/acquisio/basic/java/question03/IssueResolvingTest.java
@@ -6,8 +6,33 @@ import static org.junit.Assert.*;
 
 public class IssueResolvingTest {
     @Test
-    public void factorial() throws Exception {
+    public void factorial_returns5040_whenCalledWith7() {
         assertEquals(5040, new IssueResolving().factorial(7));
+    }
+
+    @Test
+    public void factorial_returnsOne_whenCalledWithZero() {
+        assertEquals(1, new IssueResolving().factorial(0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void factorial_throwsException_whenCalledWithNumberTooHigh() {
+        try {
+            new IssueResolving().factorial(13);
+        } catch (IllegalArgumentException e) {
+            assertEquals("This method only accepts positive integers between 1 and 12", e.getMessage());
+            throw e;
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void factorial_throwsException_whenCalledWithNegativeNumber() {
+        try {
+            new IssueResolving().factorial(-1);
+        } catch (IllegalArgumentException e) {
+            assertEquals("This method only accepts positive integers between 1 and 12", e.getMessage());
+            throw e;
+        }
     }
 
 }
